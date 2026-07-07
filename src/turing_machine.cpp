109a6778cf2bd8c2b1247367366c9ba8bd3ba159
AbolfazlAsali <asali.project006@gmail.com>
2026-07-08 00:43:12 +0330

    [feat: turing_machine.cpp] add run() and run_head()  method.

--- src/turing_machine.cpp
+++ src/turing_machine.cpp
@@ -52,3 +52,65 @@ step_info turing_machine::build_step_info() const{
 
     return info;
 }
+
+result turing_machine::run(console_renderer& renderer){
+
+    while(true){
+
+        renderer.render(build_step_info());
+
+        if(renderer.should_quit()){
+            return result::rejected;
+        }
+
+        if(accept_states.count(current_state) > 0){
+
+            renderer.set_final_result(result::accepted);
+            renderer.render(build_step_info());
+            return result::accepted;
+        }
+
+        if(rejected_states.count(current_state) > 0){
+
+            renderer.set_final_result(result::rejected);
+            renderer.render(build_step_info());
+            return result::rejected;
+        }
+
+        if(step_counter >= maximum_steps){
+
+            renderer.set_final_result(result::time_out);
+            renderer.render(build_step_info());
+            return result::time_out;
+        }
+
+        if(!step()){
+
+            renderer.set_final_result(result::rejected);
+            renderer.render(build_step_info());
+            return result::rejected;
+        }
+    }
+}
+
+result turing_machine::run_head(){
+
+    while(true){
+
+        if(accept_states.count(current_state) > 0){
+            return result::accepted;
+        }
+
+        if(rejected_states.count(current_state) > 0){
+            return result::rejected;
+        }
+
+        if(step_counter >= maximum_steps){
+            return result::time_out;
+        }
+
+        if(!step()){
+            return result::rejected;
+        }
+    }
+}
