491e9ea8711c2c17008392cc42607187ba88fab9
AbolfazlAsali <asali.project006@gmail.com>
2026-07-07 23:46:30 +0330

    [feat: turing_machine.cpp] add step() and build_step_info().

--- src/turing_machine.cpp
+++ src/turing_machine.cpp
@@ -9,11 +9,46 @@ void turing_machine::add_rejected_state(const string& input) { rejected_states.i
 
 void turing_machine::set_max_steps(int input) { maximum_steps = input; }
 
-void turing_machine::add_transition(const Transition& input) { table.addTransition(input); }
+void turing_machine::add_transition(const Transition& input) { table.add_transition(input); }
 
 void turing_machine::initial_tape(const string& input){
 
     tape.load_initial(input);
     step_counter = 0;
     current_state = start_state;
+}
+
+bool turing_machine::step(){
+
+    transition trn;
+    const char symbol = tape.read();
+    
+    if(!table.find_transition(current_state, symbol, trn)){
+        return false;
+    }
+
+
+    tape.write(trn.write_symbol);
+    if(trn.direction == 'L'){
+        tape.move_left();
+    } 
+    else{
+        tape.move_right();
+    }
+
+    current_state = trn.next_state;
+    ++step_counter;
+    return true;
+}
+
+step_info turing_machine::build_step_info() const{
+
+    step_info info;
+
+    info.state = current_state;
+    info.tape_content = tape.print();
+    info.head_position = tape.get_head_position_bounds();
+    info.step_number = step_counter;
+
+    return info;
 }
